4c4948a3544f8f3b68fc50473c6ead03e37940cf
Aman Ali <129608026+amanaliCS@users.noreply.github.com>
2025-05-02 17:48:30 -0700

    Create initial JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+let num1;
+let num2;
+let operator;
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +17,13 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1, num2);
+    }
 }
